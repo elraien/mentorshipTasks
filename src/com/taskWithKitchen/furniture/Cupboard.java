fd718b485ded6f9cf03770512b241db97805d8b4
Diana Samarska <diana.samarska@accenture.com>
2019-06-30 18:53:54 +0300

    small change in cupboard class

--- src/com/taskWithKitchen/furniture/Cupboard.java
+++ src/com/taskWithKitchen/furniture/Cupboard.java
@@ -5,16 +5,16 @@ public class Cupboard extends Furniture {
 
     @Override
     public void close() {
-
+        System.out.println("Cupboard is now closed");
     }
 
     @Override
     public void open() {
-
+        System.out.println("Cupboard is now opened");
     }
 
     @Override
     public void clean() {
-
+        System.out.println("Cupboard is now clean");
     }
 }
